5c22d8124ac811a4300a6f69c7ff2bfd4368c979
VALDEMIR ARAUJO ANDRADE <valdemiraraujo.github@gmail.com>
2025-04-05 22:15:57 -0300

    atualizei

--- exercicios/ex033projetomedia/scripts.js
+++ exercicios/ex033projetomedia/scripts.js
@@ -5,3 +5,10 @@ function cliqueMenu(){
         itens.style.display = 'block'
     }
 }
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
